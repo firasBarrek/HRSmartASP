49151e6677f9cbd19963cf1edc4b63b34d296a7f
firasBarrek <mohamedfiras.barrek@esprit.tn>
2016-11-08 17:33:54 +0100

    fill

---
+++ HRSmart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HRSmart.Startup))]
+namespace HRSmart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
